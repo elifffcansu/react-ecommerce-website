c8a2033d5e7e8ac0afbf5a9bbd6f9d75eb95fa6d
elifffcansu <elifcansu@icloud.com>
2021-10-15 10:56:51 +0300

    first commit

---
+++ src/utils/constants.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { GiCoffeeCup, GiCrownedHeart,  } from 'react-icons/gi'
+export const links = [
+  {
+    id: 1,
+    text: 'home',
+    url: '/',
+  },
+  {
+    id: 2,
+    text: 'about',
+    url: '/about',
+  },
+  {
+    id: 3,
+    text: 'products',
+    url: '/products',
+  },
+]
+
+export const services = [
+  {
+    id: 1,
+    icon: <GiCoffeeCup />,
+    title: 'mission',
+    text:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi',
+  },
+  {
+    id: 2,
+    icon: <GiCrownedHeart/>,
+    title: 'vision',
+    text:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi',
+  },
+  
+]
+
+export const products_url = "https://react-serverless-api.netlify.app/api/products" 
+
+
+export const single_product_url = `https://react-serverless-api.netlify.app/api/products?id=`
